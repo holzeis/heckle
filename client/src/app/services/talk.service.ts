6eb1f5182b277601f840214a0edef64a3b26a1a2
holzeis <richard.holzeis@gmail.com>
2019-09-01 23:23:52 +0200

    enhanced talk service

--- client/src/app/services/talk.service.ts
+++ client/src/app/services/talk.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Talk } from '../models/talk';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TalkService {
 
-  constructor() { }
+  private baseUrl: string;
+
+  public constructor(private httpClient: HttpClient) {
+    this.baseUrl = `${environment.host}/${environment.api}`;
+  }
+
+  public start(title: string): Observable<Talk> {
+    return this.httpClient.post<Talk>(this.baseUrl + '/start/', title);
+  }
+
+  public stop(talkId: string): Observable<Talk> {
+    return this.httpClient.post<Talk>(this.baseUrl + '/stop/' + talkId, {});
+  }
+
+  public loadTalks(): Observable<Talk[]> {
+    return this.httpClient.get<Talk[]>(this.baseUrl);
+  }
+
+  public loadTalk(talkId: string): Observable<Talk> {
+    return this.httpClient.get<Talk>(this.baseUrl + '/' + talkId);
+  }
 }
