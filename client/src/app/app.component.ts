bfb1292fbb11c77e975212d0e932d536a47230fb
holzeis <richard.holzeis@gmail.com>
2019-09-01 16:10:12 +0200

    added header

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+
+  public ngOnInit() {
+     Notification.requestPermission().then((result) => {
+        if (result === 'granted') {
+            const options = {
+                body: 'Hello!',
+            };
+            const notification = new Notification('Test', options);
+        }
+    });
+  }
 }
