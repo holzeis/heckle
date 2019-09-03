d9331d1661fdfb7eb547a5b1d8fc3e41f06a848b
holzeis <richard.holzeis@gmail.com>
2019-09-03 22:38:06 +0200

    added prod environment properties

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  host: 'http://heckle.localhost',
+  api: 'api/v1',
+  socket: 'ws://heckle-ws.localhost',
+  publicKey: 'QkdubDI0N21QdVcxYmhhbnZxeTJFWExWWEFjdFNJQk9lMzNUaURKODlCUzhTcjRBV2NxQ2Nqa3RKOGN1eGxMWnQtblo5bi1POHhwZFRKTVdCVnFZcXNz',
 };
