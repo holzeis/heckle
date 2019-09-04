a11909c2f871a4c76e2c00dbb1d7bf3e120826cd
holzeis <richard.holzeis@gmail.com>
2019-09-04 17:23:50 +0200

    enhanced validation

--- server/src/models/input/message.ts
+++ server/src/models/input/message.ts
@@ -1,7 +1,8 @@
-import { MaxLength } from "class-validator";
+import { MaxLength, IsNotEmpty } from 'class-validator';
 
 export class Message {
     
     @MaxLength(512)
+    @IsNotEmpty()
     public content: string;
 }
--- server/src/models/input/start.talk.ts
+++ server/src/models/input/start.talk.ts
@@ -1,6 +1,11 @@
 import { PushSubscription } from "web-push";
+import { MaxLength, IsNotEmpty } from 'class-validator';
 
 export class StartTalk {
 
-    constructor(public title: string, public subscription: PushSubscription) {}
+    @MaxLength(10)
+    @IsNotEmpty()
+    public title: string;
+
+    constructor(public subscription: PushSubscription) {}
 }
